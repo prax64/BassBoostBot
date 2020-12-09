efc83a1fc157d228c21543a826faf6ba4dd5582b
Artem Amangaliev <amon2599@gmail.com>
2020-12-09 19:55:08 +0400

    Initial commit

---
+++ BassBoost.py
@@ -0,0 +1,74 @@
+from pydub import AudioSegment
+from pydub import effects
+import numpy as np
+import math
+import os
+
+from pydub.utils import mediainfo
+
+
+
+
+def boost(name, bass, speedup):
+    track = AudioSegment.from_file(name)
+    original_bitrate = mediainfo(name)['bit_rate']
+
+    freq = bass_line_freq(track.get_array_of_samples())
+
+    if (freq < 50):
+        filtered_low = track.low_pass_filter(freq)
+        filtered_high = track.high_pass_filter(freq)
+        boosted = filtered_high.overlay(filtered_low + bass)
+        if (0.5 <= speedup <= 2):
+            boosted = speed(boosted, speedup)
+    else:
+        filtered_low = track.low_pass_filter(freq)
+        filtered_low = filtered_low + bass
+        filtered_high = track.high_pass_filter(freq)
+        for i in range(0, 2):
+            filtered_low = filtered_low.low_pass_filter(freq)
+            if not (i > 1):
+                filtered_high = filtered_high.high_pass_filter(freq)
+
+        boosted = filtered_high.overlay(filtered_low)
+        if (0.5 <= speedup <= 2):
+            boosted = speed(boosted, speedup)
+    return boosted.export(name.replace('.mp3', '') + ' [dolBitNormalno_bot].mp3', format="mp3", bitrate=original_bitrate)
+
+
+def bass_line_freq(track):
+    sample_track = list(track)
+
+    # c-value
+    est_mean = np.mean(sample_track)
+
+    # a-value
+    est_std = 3 * np.std(sample_track) / (math.sqrt(2))
+
+    bass_factor = int(round((est_std - est_mean) * 0.005))
+
+    return bass_factor
+
+
+def speed(track, speed):
+    sound_with_altered_frame_rate = track._spawn(track.raw_data, overrides={
+        "frame_rate": int(track.frame_rate * speed)
+    })
+    return sound_with_altered_frame_rate.set_frame_rate(track.frame_rate)
+
+
+def name():
+    files = os.listdir()
+    file = filter(lambda x: x.endswith('.mp3'), files)
+    return list(file)[0]
+
+
+def clean():
+    files = os.listdir()
+    file = filter(lambda x: x.endswith('.mp3'), files)
+    for i in list(file):
+        os.remove(i)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,110 @@
+import random
+import requests
+import telebot
+import config
+import dbworker
+
+from BassBoost import boost, clean
+
+bot = telebot.TeleBot(config.__token)
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('5', '10', '15', '20', '25', '30')
+
+keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard2.row('0.5', '0.6', '0.7', '0.8', '1', '1.1')
+
+def is_float(s):
+    """ Returns True is string is a number. """
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+# По команде /reset будем сбрасывать состояния, возвращаясь к началу диалога
+@bot.message_handler(commands=["reset"])
+def cmd_reset(message):
+    bot.send_message(message.chat.id, "Что ж, начнём по-новой ;)")
+    dbworker.set_state(message.chat.id, config.States.S_START.value)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Добро пожаловать!')
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAJ79V_Q2_6_mGRx6pMQaTmX3PyPRTOpAAI1AAN24i4Fls4_vHbHpAABHgQ')
+    bot.send_message(message.chat.id, 'Выбери режим буста', reply_markup=keyboard1)
+    dbworker.set_state(message.chat.id, config.States.S_ENTER_BOOST_MODE.value)
+
+
+
+@bot.message_handler(
+    func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_BOOST_MODE.value)
+def entering_boost_mode(message):
+    if not message.text.isdigit():
+        bot.send_message(message.chat.id, "Что-то не так, попробуй ещё раз!")
+        return
+    if int(message.text) < 5 or int(message.text) > 41:
+        bot.send_message(message.chat.id, "Не корректрые данные для буста")
+        return
+    else:
+        bot.send_message(message.chat.id, "Уровень буста = {0}".format(message.text))
+        config.__BOOST_MODE = int(message.text)
+        bot.send_message(message.chat.id, 'Выбери спид мода', reply_markup=keyboard2)
+        dbworker.set_state(message.chat.id, config.States.S_SEND_SPEED_MODE.value)
+
+
+@bot.message_handler(
+    func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_SEND_SPEED_MODE.value)
+def entering_speed_mode(message):
+    if not is_float(message.text):
+        bot.send_message(message.chat.id, "Что-то не так, попробуй ещё раз!")
+        return
+    if float(message.text) <= 0.5 or float(message.text) > 1.5:
+        bot.send_message(message.chat.id, "Не корректрые данные для спид мода")
+        return
+    else:
+        bot.send_message(message.chat.id, "Добавьте трек для буста")
+        config.__SPEED_MODE = float(message.text)
+        dbworker.set_state(message.chat.id, config.States.S_TRACK_PROCESSING_MODE.value)
+
+
+
+@bot.message_handler(content_types=['audio'], func=lambda message: dbworker.get_current_state(message.chat.id)
+                                                                   == config.States.S_TRACK_PROCESSING_MODE.value )
+def get_text_messages(message):
+    if message.audio:
+        file_id_info = bot.get_file(message.audio.file_id)
+        file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config.__token, file_id_info.file_path))
+        try:
+            name_track = message.audio.performer + ' - ' + message.audio.title + '.mp3'
+        except:
+            #name_track = message.json.audio.file_name
+            name_track = message.json['audio']['file_name']
+        with open(name_track, 'wb') as f:
+            f.write(file.content)
+        if message.audio.file_size // 1024 // 1024 < 10:
+            bot.send_message(message.from_user.id, "Жди, происходит обработка")
+            bot.send_audio(message.from_user.id, boost(name_track, config.__BOOST_MODE, config.__SPEED_MODE))
+
+            bot.send_message(message.from_user.id, "Кайфуй солнишка <3")
+            clean()
+        else:
+            bot.send_message(message.from_user.id,
+                             "Многа, давай что бы было меньше 10 Мб, не охота полдня твоё говно бустить")
+    elif message.text.lower() == "Привет":
+        bot.send_message(message.from_user.id, "Привет-привет-привет")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Синтаксис работы: bst 10 1.5, "
+                                               "\nгде bst - ключевое слово для работы "
+                                               "\n10 - количество децибел, на которое будет увеличен бас (не рекомендую ставить выше 40, но дело твоё, если твой гнилой таз треснет по швам или выпадет динамик, то я тебя предупреждал)"
+                                               "\n1.5 - скорость, можно не писать, если не хочешь менять скорость своего пердежа, работает от 0.5 до 2")
+    else:
+        phrases = ["Я пукнул", "Hey, nice cock", "Я хачю питси", "Эщкере", "Я какаю кусочьками"]
+        mes = random.choice(phrases) + ". Если нужна помощь - пиши /help"
+        bot.send_message(message.from_user.id, mes)
+        mes = ""
+
+
+if __name__ == '__main__':
+    bot.polling()
